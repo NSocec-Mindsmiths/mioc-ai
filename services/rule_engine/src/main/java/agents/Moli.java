df12190aedbe9f84512a817d3ae710430df0a94e
markozele1 <marko.zelenovicc@gmail.com>
2023-02-09 20:19:57 +0100

    finished up most of the armory slides, missing slider and collecting data

--- services/rule_engine/src/main/java/agents/Moli.java
+++ services/rule_engine/src/main/java/agents/Moli.java
@@ -8,6 +8,11 @@ import com.mindsmiths.armory.ArmoryAPI;
 import com.mindsmiths.armory.Screen;
 
 import com.mindsmiths.armory.component.Title;
+import com.mindsmiths.armory.component.Description;
+import com.mindsmiths.armory.component.SubmitButton;
+import com.mindsmiths.armory.component.Header;
+import com.mindsmiths.armory.component.Input;
+
 
 import java.io.IOException;
 import java.util.List;
@@ -18,17 +23,62 @@ import com.mindsmiths.emailAdapter.EmailAdapterAPI;
 @ToString(callSuper = true)
 @NoArgsConstructor
 public class Moli extends Agent {
+    String feeback;
     public void showHelloScreen() {
         ArmoryAPI.show(
             getConnection("armory"),
             new Screen ("Welcome")
-                .add(new Header("mioc_logo 3.png", true)) // trebam nekako uploadati logo sa kompa
+                .add(new Header("logo.png", false))
                 .add(new Title("Bok, ja sam Moli! Može anketa?"))
-                .add(new Text("Hvala ti puno što mi pomažeš <3"))
+                .add(new Description("Hvala ti puno što mi pomažeš <3"))
                 .add(new SubmitButton("welcomeStarted", "Idemo!", "askForGender")),
-            new Screen ("AskForGender")
-                .add(new Header("mioc_logo 3.png", true)) 
-                .add(new Title("Jesi li miočanin ili miočanka?")) //trebam dodati dva gumba koji pokrecu drukcije pravilo tako da se prilagodi spol
+            new Screen ("askForGender")
+                .add(new Header("logo.png", true)) 
+                .add(new Title("Jesi li miočanin ili miočanka?")) 
+                .add(new SubmitButton("askForGenderStarted", "Miočanin", "askForAgem")) //uhvati podatke i salji Ravnatelj agentu
+                .add(new SubmitButton("askForGenderStarted", "Miočanka", "askForAgef")),
+            new Screen ("askForAgem") //postoje m i f verzije stranica sa prilagodenim recenicama s obzirom na spol
+                .add(new Header("logo.png", true)) 
+                .add(new Title("Koji si razred?"))
+                .add(new SubmitButton("askForAgemStarted", "Prvi", "askForRatingm")) //uhvati podatke i salji Ravnatelj agentu
+                .add(new SubmitButton("askForAgemStarted", "Drugi", "askForRatingm"))
+                .add(new SubmitButton("askForAgemStarted", "Treći", "askForRatingm"))
+                .add(new SubmitButton("askForAgemStarted", "Četvrti", "askForRatingm")),
+            new Screen ("askForAgef")
+                .add(new Header("logo.png", true)) 
+                .add(new Title("Koji si razred?"))
+                .add(new SubmitButton("askForAgefStarted", "Prvi", "askForRatingf")) //uhvati podatke i salji Ravnatelj agentu
+                .add(new SubmitButton("askForAgefStarted", "Drugi", "askForRatingf"))
+                .add(new SubmitButton("askForAgefStarted", "Treći", "askForRatingf"))
+                .add(new SubmitButton("askForAgefStarted", "Četvrti", "askForRatingf")),
+            new Screen ("askForRatingm")
+                .add(new Header("logo.png", true)) 
+                .add(new Title("Preporučuješ li MIOC?"))
+                .add(new Description("Kolika je vjerojatnost da bi preporučio MIOC frendu/frendici?"))
+                .add(new SubmitButton("askForRatingmStarted", "Idemo!", "askForFeedbackm")), //dodaj slider
+            new Screen ("askForRatingf")
+                .add(new Header("logo.png", true)) 
+                .add(new Title("Preporučuješ li MIOC?"))
+                .add(new Description("Kolika je vjerojatnost da bi preporučila MIOC frendu/frendici?"))
+                .add(new SubmitButton("askForRatingfStarted", "Idemo!", "askForFeedbackf")), //dodaj slider
+            new Screen ("askForFeedbackm")
+                .add(new Header("logo.png", true))
+                .add(new Title("Što bi moglo biti bolje?"))
+                .add(new Description("Napiši koje promjene bi škola morala uvesti da bi joj dao veću ocjenu?"))
+                .add(new Input("feedback", "Napiši svoj kometar...", "text"))
+                .add(new SubmitButton("askForFeedbackmStarted", "Pošalji", "endScreen")),
+            new Screen ("askForFeedbackf")
+                .add(new Header("logo.png", true))
+                .add(new Title("Što bi moglo biti bolje?"))
+                .add(new Description("Napiši koje promjene bi škola morala uvesti da bi joj dala veću ocjenu?"))
+                .add(new Input("feedback", "Napiši svoj kometar...", "text"))
+                .add(new SubmitButton("askForFeedbackfStarted", "Pošalji", "endScreen")),
+            new Screen ("endScreen")
+                .add(new Header("logo.png", false))
+                .add(new Title("Hvala ti! I tvoje mišljenje je bitno."))
+                .add(new Description("Zajedno ćemo učiniti MIOC boljim <3"))
+
+
         );
         try {
             sendEmail(List.of("marko.zelenovicc@gmail.com"), "Test title", "Test 12345");
